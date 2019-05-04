1f7716db362dc5a3c32561f22710bdd02425d519
Ryan Blecher <notryanb@gmail.com>
2019-05-04 12:38:33 -0400

    Add some comments regarding usage api

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,45 @@ use std::path::Path;
 use structopt::StructOpt;
 use walkdir::{DirEntry, WalkDir};
 
-/* 
-    TODO:
-
-*/
-
 #[derive(Debug, StructOpt)]
 struct Cli {
     // The absolute filepath you want to import all mp3s
-    directory: String,
+    track_source: String,
 
     #[structopt(flatten)]
     verbosity: Verbosity,
 }
 
+
+/*
+    Example usege
+    ---
+    - Get default track info (artist, album, track title, year): `tmt ./song.mp3`
+    - Get artist on track: `tmt ./song.mp3 --artist`
+    - Get artist and album on track: `tmt ./song.mp3 --artist --album`
+    - Set artist on track: `tmt ./song.mp3 --artist=Fugazi`
+    - Set artist and album on track: `tmt ./song.mp3 --artist="Fugazi" --album="13 Songs"`
+
+    All Sets for a directory should confirm "are you sure you want the following settings for all
+    mp3 tracks in the following directory {}?"
+    - Get default track info for each in directory (artist, album, track title, year): `tmt ./Fugazi`
+    - Get artist on each track in directory: `tmt ./Fugazi --artist`
+    - Get artist and album on track in directory: `tmt ./Fugazi --artist --album`
+    - Set artist on track: `tmt ./Fugazi --artist=Fugazi`
+    - Set artist and album on track: `tmt ./Fugazi --artist="Fugazi" --album="13 Songs"`
+*/
+
 fn main() -> CliResult {
     let args = Cli::from_args();
 
-    if !Path::new(&args.directory).exists() {
-        warn!("Error: {:?} is not a valid path", args.directory);
+    if !Path::new(&args.track_source).exists() {
+        warn!("Error: {:?} is not a valid path", args.track_source);
     }
 
     let mut file_count = 0.0;
-    let mp3_files = get_all_files_in_directory(&args.directory);
+    let mp3_files = get_all_files_in_directory(&args.track_source);
     let total_files = mp3_files.clone().into_iter().count() as f32;
-    
+
     println!("Total Files: {}", &total_files);
 
     for e in mp3_files.into_iter() {
